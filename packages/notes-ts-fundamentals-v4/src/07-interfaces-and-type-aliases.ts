08616499f7dd38b092617367cf8532c15622876e
Simone Taeggi <startae14@gmail.com>
2024-02-28 00:50:26 +0100

    Open Interfaces

--- packages/notes-ts-fundamentals-v4/src/07-interfaces-and-type-aliases.ts
+++ packages/notes-ts-fundamentals-v4/src/07-interfaces-and-type-aliases.ts
@@ -172,15 +172,14 @@ class Dog3 implements CanJump, CanBark2 {
 
 //* Open interfaces
 
-/*
-// function feed(animal: AnimalLike) {
-//     animal.eat
-//     animal.isAlive
-// }
-/*
-// interface AnimalLike { //✔️ Additional declaration is OK
-//     isAlive(): boolean
-// }
+function feed(animal: AnimalLike) {
+  if (animal.isAlive()) animal.eat('food')
+}
+
+interface AnimalLike {
+  //✔️ Additional declaration is OK
+  isAlive(): boolean
+}
 
 //* Use case: augmenting existing types
 
