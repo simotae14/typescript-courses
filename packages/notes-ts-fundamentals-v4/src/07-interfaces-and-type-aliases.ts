79156f8ce6af8be866f1cc390a36f77e6ba0c954
Simone Taeggi <startae14@gmail.com>
2024-02-28 01:05:06 +0100

    recursive types, example

--- packages/notes-ts-fundamentals-v4/src/07-interfaces-and-type-aliases.ts
+++ packages/notes-ts-fundamentals-v4/src/07-interfaces-and-type-aliases.ts
@@ -201,6 +201,8 @@ const val: NestedNumbers = [3, 4, [5, 6, [7], 59], 221]
 
 if (typeof val !== 'number') {
   val.push(41)
+  val.push([[41, 32], 11])
+  val.push([[]])
   val.push('this will not work') //! No strings allowed
 }
 
